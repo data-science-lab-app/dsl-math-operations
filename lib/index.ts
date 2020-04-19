bed537967be338bc50f65289644910f3d2695baf
CobeGreene <cobegreene@gmail.com>
2020-04-19 13:52:20 -0700

    Export classes in index

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,6 @@
 export { AddFeaturesOperation } from './add-features.operation';
 export { AddOperation } from './add.operation';
+export { MultiplyOperation } from './multiply.operation';
+export { MultiplyFeaturesOperation } from './multiply-features.operation';
+export { ModulusOperation } from './modulus.operation';
+export { ExponentiateOperation } from './exponentiate.operation';
